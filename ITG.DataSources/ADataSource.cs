95e12665756cf1289264b7de63de457785992ce8
Graeme Mulvaney <web.bod@gmail.com>
2020-08-11 15:19:37 +0100

    Migrated DataSources

---
+++ ITG.DataSources/ADataSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ITG.Interfaces;
+using ITG.Models.Entities;
+using ITG.Models.MetaData;
+using ITG.Models.Configuration;
+
+namespace ITG.DataSources
+{
+    public abstract class ADataSource : IDataSource
+    {
+        protected ArticleListMetaData _MetaData;
+
+        public ArticleListMetaData MetaData => _MetaData;
+
+        public abstract List<Article> GetPage(int pageNumber = 0);
+    
+        public abstract void Configure(DataSourceConfiguration config);
+
+        // this is public, but not included in IDataSource - there may be odd cases
+        // where a consumer needs to explicitly invoke Load, but it will be obvious
+        // in the code where this is happening and should be discouraged
+        public abstract void Load();
+    }
+}
---
+++ ITG.DataSources/FileDataSource.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using ITG.Models.Entities;
+using System.Text.Json;
+using ITG.Models.MetaData;
+using ITG.Models.Configuration;
+
+namespace ITG.DataSources
+{
+    public class FileDataSource : ADataSource
+    {
+        private string _Path = String.Empty;
+
+        private List<Article> _Data;
+
+        public FileDataSource() : base()
+        {
+        }
+
+        private void ConfigurePath(DataSourceConfiguration config)
+        {
+            // ensure the path to the file is in the correct format
+            var path = config.ConnectionSettings.Replace('/', '\\');
+            path = path.Substring(0, 1) == "~" ? path.Substring(1) : path;
+            path = path.Substring(0, 1) == "\\" ? path.Substring(1) : path;
+
+            _Path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
+
+            if (!File.Exists(_Path))
+            {
+                
+                throw new FileNotFoundException($"The data file was not found at :{_Path}");
+            }
+        }
+
+        public override void Configure(DataSourceConfiguration config)
+        {
+            ConfigurePath(config);
+            _MetaData = new ArticleListMetaData { PageSize = config.PageSize };
+        }
+
+        public override List<Models.Entities.Article> GetPage(int pageNumber = 0)
+        {
+            if (_Data == default(List<Article>) || !_Data.Any())
+            {
+                Load();
+            }
+
+            return _Data.Skip(pageNumber * _MetaData.PageSize).Take(_MetaData.PageSize).ToList();
+        }
+
+        public override void Load()
+        {
+            using (StreamReader r = new StreamReader(_Path))
+            {
+                string json = r.ReadToEnd();
+
+                // switched to using native dotnet core JsonSerializer
+                _Data = JsonSerializer.Deserialize<List<Article>>(json);
+            }
+
+            _MetaData.ArticleCount = _Data.Count();
+        }
+    }
+}
