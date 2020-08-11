f9881a5ff4a79c8bfc87704a25995e9f1e703d6c
Graeme Mulvaney <web.bod@gmail.com>
2020-08-11 15:19:48 +0100

    Migrated Interfaces

---
+++ ITG.Interfaces/IDataSource.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ITG.Models.Entities;
+using ITG.Models.MetaData;
+using ITG.Models.Configuration;
+
+namespace ITG.Interfaces
+{
+    public interface IDataSource
+    {
+        /// <summary>
+        /// Describes the datasource
+        /// </summary>
+        ArticleListMetaData MetaData { get; }
+
+        /// <summary>
+        /// Pages through the dataset
+        /// </summary>
+        /// <param name="pageNumber">requested page</param>
+        /// <returns>a page worth of records</returns>
+        List<Article> GetPage(int pageNumber = 0);
+
+        /// <summary>
+        /// Configures the datasource
+        /// </summary>
+        /// <param name="config">configuration data</param>
+        void Configure(DataSourceConfiguration config);
+    }
+}
