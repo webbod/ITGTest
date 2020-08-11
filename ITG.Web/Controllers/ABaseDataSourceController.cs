1632d81771c59e3fc8d7db6505333dccd65d6850
Graeme Mulvaney <web.bod@gmail.com>
2020-08-11 11:23:07 +0100

    Moved common functionality into ABaseDataSourceController and simplified NewsFeedController - removed the redundant NewsFeedViewModel

---
+++ ITG.Web/Controllers/ABaseDataSourceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITG.Interfaces;
+using ITG.Models.Configuration;
+using System.Configuration;
+
+
+namespace ITG.Web.Controllers
+{
+    public abstract class ABaseDataSourceController : Controller
+    {
+        protected IDataSource _DataSource;
+
+        public ABaseDataSourceController(IDataSource dataSource)
+        {
+            ConfigureDataSource(dataSource);            
+        }
+
+        protected void ConfigureDataSource(IDataSource dataSource)
+        {
+            // get the configuration infromation from web.config
+            var connectionSettings = ConfigurationManager.AppSettings["DataSource.ConnectionSettings"];
+            var pageSize = Int32.Parse(ConfigurationManager.AppSettings["DataSource.PageSize"]);
+
+            if (string.IsNullOrEmpty(connectionSettings) || pageSize < 1)
+            {
+                throw new ConfigurationErrorsException();
+            }
+
+            var config = new DataSourceConfiguration { ConnectionSettings = connectionSettings, PageSize = pageSize };
+
+            dataSource.Configure(config);
+
+            _DataSource = dataSource;
+        }
+    }
+}
--- ITG.Web/Controllers/NewsFeedController.cs
+++ ITG.Web/Controllers/NewsFeedController.cs
@@ -3,42 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ITG.Interfaces;
-using ITG.Models.Entities;
-using ITG.Models.MetaData;
 using Newtonsoft.Json;
-using ITG.Web.Models;
-using ITG.Models.Configuration;
-using System.Configuration;
-using System.IO;
+using ITG.Interfaces;
 
 namespace ITG.Web.Controllers
 {
-    public class NewsFeedController : Controller
+    public class NewsFeedController : ABaseDataSourceController
     {
-        private IDataSource _DataSource;
 
-        private void ConfigureDataSource(IDataSource dataSource)
+
+        public NewsFeedController(IDataSource dataSource) : base(dataSource)
         {
-            // get the configuration infromation from web.config
-            var connectionSettings = ConfigurationManager.AppSettings["DataSource.ConnectionSettings"];
-            var pageSize = Int32.Parse(ConfigurationManager.AppSettings["DataSource.PageSize"]);
-
-            if (string.IsNullOrEmpty(connectionSettings) || pageSize < 1)
-            {
-                throw new ConfigurationErrorsException();
-            }
-
-            var config = new DataSourceConfiguration { ConnectionSettings = connectionSettings, PageSize = pageSize };
-
-            dataSource.Configure(config);
-
-            _DataSource = dataSource;
-        }
-
-        public NewsFeedController(IDataSource dataSource)
-        {
-            ConfigureDataSource(dataSource);            
         }
 
         //
@@ -46,15 +21,7 @@ namespace ITG.Web.Controllers
 
         public ActionResult Index()
         {
-            var startingPage = 0;
-
-            var model = new NewsFeedViewModel
-            {
-                CurrentPage = startingPage,
-                MetaData = _DataSource.MetaData
-            };
-
-            return View(model);
+            return View();
         }
 
         //
@@ -62,19 +29,10 @@ namespace ITG.Web.Controllers
 
         public JsonResult Page(int id = 0)
         {
-            IEnumerable<Article> output = null;
-            
-            output = _DataSource.GetPage(id);
+            var output = _DataSource.GetPage(id);
             
             return Json(output, JsonRequestBehavior.AllowGet);
         }
 
-        //
-        // GET: /NewsFeed/MetaData
-
-        public JsonResult MetaData()
-        {
-            return Json(_DataSource.MetaData, JsonRequestBehavior.AllowGet);
-        }
     }
 }
