ef4a3a4999373105a1a4882d596a1283068e0e4f
Graeme Mulvaney <web.bod@gmail.com>
2020-08-11 15:20:15 +0100

    Migrated Controllers

---
+++ ITG.Web/Controllers/ABaseDataSourceController.cs
@@ -0,0 +1,34 @@
+﻿using ITG.Interfaces;
+using ITG.Models.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ITG.Web.Controllers
+{
+    public abstract class ABaseDataSourceController : Controller
+    {
+        protected IDataSource _DataSource;
+
+        public ABaseDataSourceController(IDataSource dataSource, IConfiguration configuration)
+        {
+            ConfigureDataSource(dataSource, configuration);            
+        }
+
+        protected void ConfigureDataSource(IDataSource dataSource, IConfiguration configuration)
+        {
+            // get the configuration infromation from web.config
+            var connectionSettings = configuration.GetSection("DataSource").GetSection("ConnectionSettings").Value;
+            var pageSize = Int32.Parse(configuration.GetSection("DataSource").GetSection("PageSize").Value);
+
+            if (string.IsNullOrEmpty(connectionSettings) || pageSize < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            dataSource.Configure(new DataSourceConfiguration { ConnectionSettings = connectionSettings, PageSize = pageSize });
+
+            _DataSource = dataSource;
+        }
+    }
+}
---
+++ ITG.Web/Controllers/NewsFeedController.cs
@@ -0,0 +1,28 @@
+﻿using ITG.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ITG.Web.Controllers
+{
+    public class NewsFeedController : ABaseDataSourceController
+    {
+
+
+        public NewsFeedController(IDataSource dataSource, IConfiguration configuration) : base(dataSource, configuration)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Page(int id = 0)
+        {
+            var output = _DataSource.GetPage(id);
+
+            return Json(output);
+        }
+
+    }
+}
